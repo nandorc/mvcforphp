dba021c9d9219e5d1f3566e143325ea57fb26505
NandoRC <tutordesoftware@gmail.com>
2020-03-10 15:16:13 -0500

    New Version
    Adjusted for HTML5 Semantics

---
+++ mvcforphp_v2/baseController.php
@@ -0,0 +1,27 @@
+<?php
+//Models required in the Controller
+//called by the instruction
+//   include_once "../models/[NombreModelo].php";
+//
+//Begin the session environment
+session_start();
+//
+//Validate and load actions
+//To redirect, use the instruction header like is show putting the name of the view
+//    header("Location: ../[view]");
+if (!isset($_GET["action"])) {
+    header("Location: ../index.php");
+} else {
+    $action = $_GET["action"];
+    //Add each action as cases for the switch conditional
+    //if you use any Database action, use a try...catch block to consider exceptions
+    try {
+        switch ($action) {
+            default:
+                throw new Exception("No action or no valid action sent");
+        }
+    } catch (Exception $ex) {
+        //Display or send error message according to your needs
+        echo "Error: " . $ex->getMessage();
+    }
+}
