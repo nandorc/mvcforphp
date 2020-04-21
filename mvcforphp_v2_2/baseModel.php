1ad355600f5428077e4e0800876a953f47b28f15
nandorc <60071444+nandorc@users.noreply.github.com>
2020-04-21 05:18:08 -0500

    Update baseModel.php

--- mvcforphp_v2_2/baseModel.php
+++ mvcforphp_v2_2/baseModel.php
@@ -4,8 +4,8 @@ class ModelName extends DBModel
     private $name = "";
     private $fields = array();
     private $pk = "";
-    public function __construct()
+    public function __construct(int $level = 1)
     {
-        parent::__construct(new SQLTable($this->name, $this->fields, $this->pk));
+        parent::__construct(new SQLTable($this->name, $this->fields, $this->pk), $level);
     }
 }
