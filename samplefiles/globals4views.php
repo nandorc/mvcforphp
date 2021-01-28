4d55850c7190c4494b03ebc1b5ff7502f1a64059
nandorc <60071444+nandorc@users.noreply.github.com>
2021-01-28 15:26:58 -0500

    Update on samples

--- samplefiles/globals4views.php
+++ samplefiles/globals4views.php
@@ -1,1 +1,8 @@
 <?php
+const TEMPLATE_DEFAULT_TITLE = "";
+const TEMPLATE_DESCRIPTION = "";
+const TEMPLATE_KEYWORDS = "";
+const TEMPLATE_AUTHOR = "";
+const TEMPLATE_ICON = "";
+const TEMPLATE_CSS = null;
+const TEMPLATE_JS = null;
--- samplefiles/template.php
+++ samplefiles/template.php
@@ -3,6 +3,7 @@
 use MVC4PHP\View;
 
 $title = isset($data["title"]) ? $data["title"] : "";
+$pagetitle = (TEMPLATE_DEFAULT_TITLE == "") ? $title : (($title == "") ? TEMPLATE_DEFAULT_TITLE : TEMPLATE_DEFAULT_TITLE . " - " . $title);
 $css = isset($data["css"]) ? $data["css"] : null;
 $js = isset($data["js"]) ? $data["js"] : null;
 ?>
@@ -12,16 +13,18 @@ $js = isset($data["js"]) ? $data["js"] : null;
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="keywords" content="">
-    <meta name="author" content="">
-    <title><?= $title; ?></title>
-    <link rel="icon" href="">
+    <meta name="description" content="<?= TEMPLATE_DESCRIPTION; ?>">
+    <meta name="keywords" content="<?= TEMPLATE_KEYWORDS; ?>">
+    <meta name="author" content="<?= TEMPLATE_AUTHOR; ?>">
+    <title><?= $pagetitle; ?></title>
+    <link rel="icon" href="<?= TEMPLATE_ICON; ?>">
+    <?= View::includeexternals(TEMPLATE_CSS, "style"); ?>
     <?= View::includeexternals($css, "style"); ?>
 </head>
 
 <body>
     <?= $content(); ?>
+    <?= View::includeexternals(TEMPLATE_JS, "script"); ?>
     <?= View::includeexternals($js, "script"); ?>
 </body>
 
